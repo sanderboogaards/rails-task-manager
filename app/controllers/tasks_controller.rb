75c8ac7daf024cfbc372a12c6d1169c9fc1cf771
Sander Boogaards <sanderboogaards@gmail.com>
2021-08-17 12:08:34 +0200

    WIP Rails Task Manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
