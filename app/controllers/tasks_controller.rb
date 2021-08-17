f6b105083feb66e514f1a14305f22702d6edad00
Sander Boogaards <sanderboogaards@gmail.com>
2021-08-17 14:38:15 +0200

    Solution rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,14 +24,14 @@ class TasksController < ApplicationController
 
   def update
     # @task = Task.find(params[:id])
-		@task.update(task_params)
-		redirect_to task_path(@task)
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
     # @task = Task.find(params[:id])
-		@task.destroy
-		redirect_to tasks_path
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
